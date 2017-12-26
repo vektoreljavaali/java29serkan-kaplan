119ba66dcb121adb7f628db7dd5bbaa9cc79e363
Serkan <Serkan@LAPTOP-5T60BOVE>
2017-12-26 23:48:29 +0300

    frmegitici ilk kurulum.

---
+++ src/egiticiMasaustu/FrmEgitici.java
@@ -0,0 +1,43 @@
+package egiticiMasaustu;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class FrmEgitici {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmEgitici window = new FrmEgitici();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FrmEgitici() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
